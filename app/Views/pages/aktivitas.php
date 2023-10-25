78a3b848bf8e917c46f7f315f299f2741a32605d
Izaz Dhiyaulhaq <id.izaz123@gmail.com>
2023-10-25 22:45:04 +0700

    refactor(views): Perbaikan ui

--- app/Views/pages/aktivitas.php
+++ app/Views/pages/aktivitas.php
@@ -2,9 +2,5 @@
 
 <?= $this->section('content') ?>
 
-<h2 class="h2">Komunitas Codeigniter Indonesia Mengadakan Meetup</h2>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-<h2 class="h2">Dukungan Keamanan untuk Codeigniter 3 akan barakhir tahun ini</h2>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 
 <?= $this->endSection() ?>
